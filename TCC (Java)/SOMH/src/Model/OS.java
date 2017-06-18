0d34b0f09cb5b01dc19b7b3bff47fae1ebb2230c
Victor Gabriel <victorgabrielvgfm@gmail.com>
2017-06-18 18:59:03 -0300

    Atualiza repositorio

---
+++ TCC (Java)/SOMH/src/Model/OS.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author andro
+ */
+public class OS {
+    private int nro_OS;
+    private int cod_Cpf_Cnpj;
+    private int seq_Equipto;
+    private String txt_Reclamacao;
+    private double vlr_Desconto;
+    private double per_Desconto;
+    private double vlr_Frete;
+
+    public OS() {
+    }
+    
+    public OS(int nro_OS, int cod_Cpf_Cnpj, int seq_Equipto, String txt_Reclamacao, double vlr_Desconto, double per_Desconto, double vlr_Frete) {
+        this.nro_OS = nro_OS;
+        this.cod_Cpf_Cnpj = cod_Cpf_Cnpj;
+        this.seq_Equipto = seq_Equipto;
+        this.txt_Reclamacao = txt_Reclamacao;
+        this.vlr_Desconto = vlr_Desconto;
+        this.per_Desconto = per_Desconto;
+        this.vlr_Frete = vlr_Frete;
+    }
+
+    public int getNro_OS() {
+        return nro_OS;
+    }
+
+    public void setNro_OS(int nro_OS) {
+        this.nro_OS = nro_OS;
+    }
+
+    public int getCod_Cpf_Cnpj() {
+        return cod_Cpf_Cnpj;
+    }
+
+    public void setCod_Cpf_Cnpj(int cod_Cpf_Cnpj) {
+        this.cod_Cpf_Cnpj = cod_Cpf_Cnpj;
+    }
+
+    public int getSeq_Equipto() {
+        return seq_Equipto;
+    }
+
+    public void setSeq_Equipto(int seq_Equipto) {
+        this.seq_Equipto = seq_Equipto;
+    }
+
+    public String getTxt_Reclamacao() {
+        return txt_Reclamacao;
+    }
+
+    public void setTxt_Reclamacao(String txt_Reclamacao) {
+        this.txt_Reclamacao = txt_Reclamacao;
+    }
+
+    public double getVlr_Desconto() {
+        return vlr_Desconto;
+    }
+
+    public void setVlr_Desconto(double vlr_Desconto) {
+        this.vlr_Desconto = vlr_Desconto;
+    }
+
+    public double getPer_Desconto() {
+        return per_Desconto;
+    }
+
+    public void setPer_Desconto(double per_Desconto) {
+        this.per_Desconto = per_Desconto;
+    }
+
+    public double getVlr_Frete() {
+        return vlr_Frete;
+    }
+
+    public void setVlr_Frete(double vlr_Frete) {
+        this.vlr_Frete = vlr_Frete;
+    }
+    
+}
