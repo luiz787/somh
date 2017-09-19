975fe174cba8249f7ea7a23548fd2f4e7fedce62
Victor César de Paula Rangel <playcontasvictor2@gmail.com>
2017-09-19 01:16:40 -0300

    Melhora controlador

--- TCC (Java)/SOMH/src/Controller/CadastroUsuarioController.java
+++ TCC (Java)/SOMH/src/Controller/CadastroUsuarioController.java
@@ -5,12 +5,15 @@
  */
 package Controller;
 
+import DAOImpl.UsuarioDAOImpl;
 import Domain.Perfil;
 import Domain.Usuario;
 import Exception.ExcecaoNegocio;
 import Exception.ExcecaoPersistencia;
 import Main.Run;
+import Service.ManterPerfil;
 import Service.ManterUsuario;
+import ServiceImpl.ManterPerfilImpl;
 import ServiceImpl.ManterUsuarioImpl;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +23,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
@@ -63,8 +67,8 @@ public class CadastroUsuarioController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
     public void setRun(Run run) {
         this.run = run;
     }
@@ -74,58 +78,106 @@ public class CadastroUsuarioController implements Initializable {
         btnCadastrarCliente.setBackground(new Background(new BackgroundFill(Paint.valueOf("#eff1f4"), CornerRadii.EMPTY, Insets.EMPTY)));
         btnCadastrarCliente.setBorder(new Border(new BorderStroke(Paint.valueOf("#eff1f4"), BorderStrokeStyle.NONE, CornerRadii.EMPTY, BorderWidths.EMPTY)));
     }
-    
+
     @FXML
     private void mouseClick(ActionEvent event) {
         System.out.println("test");
         btnCadastrarCliente.setBackground(new Background(new BackgroundFill(Paint.valueOf("#d0d2d8"), CornerRadii.EMPTY, Insets.EMPTY)));
         btnCadastrarCliente.setBorder(new Border(new BorderStroke(Paint.valueOf("#d0d2d8"), BorderStrokeStyle.NONE, CornerRadii.EMPTY, BorderWidths.EMPTY)));
     }
-    
+
     @FXML
-    private void confirma() {
-        if(!validacao() && txt_senha.getText() != confirmaSenha.getText()) {
-            JOptionPane.showMessageDialog(null, "Digite um nome de usuario!!!", "Erro no cadastro", JOptionPane.ERROR_MESSAGE);
+    private void cadastraUsuario() {
+        if (!validacao() && txt_senha.getText() != confirmaSenha.getText()) {
+            try {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Cadastro de Usuario");
+                alert.setHeaderText("Erro de cadastro");
+                alert.setContentText("As senhas nao sao compativeis!!!");
+
+                alert.showAndWait();
+                
+                System.out.println("Deu merda, senha n bate");
+                
+                throw new Exception("A confirmacao da senha esta errada!");
+            } catch (Exception ex) {
+                Logger.getLogger(CadastroUsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        
-        if(!validacao() && txt_senha.getText() != confirmaSenha.getText()) {
-            JOptionPane.showMessageDialog(null, "As senhas não correspondem!!!", "Erro no cadastro" ,JOptionPane.ERROR_MESSAGE);
+
+        if (!validacao()) {
+            try {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Cadastro de Usuario");
+                alert.setHeaderText("Erro de cadastro");
+                alert.setContentText("Preencha todos os campos!!!");
+
+                alert.showAndWait();
+                
+                System.out.println("Deu merda, falto campo");
+                
+                throw new Exception("Preencha os campos!");
+            } catch (Exception ex) {
+                Logger.getLogger(CadastroUsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        
-        if(validacao()) {
-            
+
+        if (validacao()) {
+            try {
+                ManterUsuario manterUser = new ManterUsuarioImpl();
+                ManterPerfil manterPefil = new ManterPerfilImpl();
+                Usuario user = new Usuario();
+                Perfil perfil = new Perfil();
+                
+                user.setNome(nom_usuario.getText());
+                user.setSenha(txt_senha.getText());
+                
+                perfil.setDescricao(des_perfil.getText());
+                
+                perfil.setId(Long.MIN_VALUE);
+                user.setPerfil(perfil);
+                user.setId(manterUser.cadastrar(user));
+                
+                System.out.println("cole zeze, deu certo");
+                
+            } catch (ExcecaoPersistencia ex) {
+                System.out.println("Excecao de persistencia!");
+            } catch (ExcecaoNegocio ex) {
+                System.out.println("Excecao de negocio!");
+
+            }
         }
     }
-    
+
     @FXML
     private void sair() {
-        
+
     }
-    
+
     private boolean validacao() {
-        
+
         boolean validacao = true;
-        
-        if(nom_usuario.getText().isEmpty()) {
+
+        if (nom_usuario.getText().isEmpty()) {
             validacao = false;
         }
-        
-        if(txt_senha.getText().isEmpty()) {
+
+        if (txt_senha.getText().isEmpty()) {
             validacao = false;
         }
-        
-        if(confirmaSenha.getText().isEmpty()) {
+
+        if (confirmaSenha.getText().isEmpty()) {
             validacao = false;
         }
-        
-        if(des_perfil.getText().isEmpty()) {
+
+        if (des_perfil.getText().isEmpty()) {
             validacao = false;
         }
-        
-        if(txt_senha.getText() != confirmaSenha.getText()) {
+
+        if (txt_senha.getText() != confirmaSenha.getText()) {
             validacao = false;
         }
-        
+
         return validacao;
     }
 }
